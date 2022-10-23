d212d591986d0b683c3df89be99f2a8f4b8e01ba
Tom Court <Tomcourt1989@bitbucket.org>
2022-10-24 00:47:34 +0100

    layouts finished, js menu animations added

--- javascript/site.js
+++ javascript/site.js
@@ -1,1 +1,16 @@
 // Place your general js functionality here should you need to
+// Function to give the active class to elements when the toggle (hamburger menu) is clicked
+window.onload = function() {
+    const menuToggle = document.querySelector('.toggle');
+    const navbar = document.querySelector('.navbar');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle("active");
+        navbar.classList.toggle("active");
+    });
+
+    // Sets the active class as default when on desktop sized window
+    if(window.innerWidth > 1250) {
+        navbar.classList.toggle("active");
+    }
+}
