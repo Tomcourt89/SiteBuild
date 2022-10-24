73c6d100e2323eebb1fc16e87142b6da7d42da13
Tom Court <Tomcourt1989@bitbucket.org>
2022-10-24 04:59:08 +0100

    Test page complete, JS added for menus and gallery slider.

--- javascript/site.js
+++ javascript/site.js
@@ -1,6 +1,27 @@
 // Place your general js functionality here should you need to
-// Function to give the active class to elements when the toggle (hamburger menu) is clicked
+
+// Array with gallery images + their titles
+const slides = [
+    { 
+        imgSrc: "images/img_maingallery_couplemountains.jpg",
+        h1: "Explore the breathtaking mountain ranges",
+    },
+    
+    { 
+        imgSrc: "images/img_maingallery_manonrock.jpg",
+        h1: "The best view comes after the hardest climb",
+    },
+
+    { 
+        imgSrc: "images/img_maingallery_streetlights.jpg",
+        h1: "Sit back and watch the world go by",
+    },
+];
+
+let currentSlide = 0;
+
 window.onload = function() {
+    // Event to give the active class to elements when the toggle (hamburger menu) is clicked
     const menuToggle = document.querySelector('.toggle');
     const navbar = document.querySelector('.navbar');
 
@@ -11,6 +32,45 @@ window.onload = function() {
 
     // Sets the active class as default when on desktop sized window
     if(window.innerWidth > 1250) {
-        navbar.classList.toggle("active");
+        navbar.classList.add("active");
     }
-}
+
+    // Closes any open menus when the browser is resized.
+    window.addEventListener('resize', () => {
+        navbar.classList.remove("active");
+    });
+
+    // Dynamically fills in the image src and replaces the heading + cycle through
+    const slide = document.querySelector(".slideimg");
+    const title = document.querySelector(".slidetitle");
+    const prev = document.querySelector(".leftarrow");
+    const next = document.querySelector(".rightarrow");
+
+    slide.src = slides[currentSlide].imgSrc;
+    title.textContent = slides[currentSlide].h1;
+    
+    
+    prev.addEventListener('click', () => {
+        currentSlide--;
+        if (currentSlide < 0) {
+            currentSlide = slides.length -1;
+        }
+        slide.src = slides[currentSlide].imgSrc;
+        title.textContent = slides[currentSlide].h1;
+    });
+    next.addEventListener('click', () => {
+        currentSlide++;
+        if (currentSlide > slides.length - 1) {
+            currentSlide = 0;
+        }
+        slide.src = slides[currentSlide].imgSrc;
+        title.textContent = slides[currentSlide].h1;
+    });
+};
+
+
+
+
+
+
+
